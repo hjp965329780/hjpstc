67448ae8545ff77e1c35fcf26316ca7fdb9fbb1a
huangjinpeng <huangjinpeng@kuaishou.com>
2025-02-06 10:07:08 +0800

    feat: 更新随机取数

--- packages/personal-summary/luck.js
+++ packages/personal-summary/luck.js
@@ -1,7 +1,7 @@
-// 1-10随机取n个数
-function getRandomNumbers(n) {
-    // 创建一个包含1到10的数组
-    const numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+// 1-m中随机取n个数
+function getRandomNumbers(m, n) {
+    // 创建一个包含1到m的数组
+    const numbers = Array.from({ length: m }, (_, i) => i + 1);
     
     // 打乱数组顺序
     for (let i = numbers.length - 1; i > 0; i--) {
