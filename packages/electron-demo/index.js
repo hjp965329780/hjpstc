05ba1d3a805d823ee4e27cc9cd698172fe3cfa30
huangjinpeng <huangjinpeng@kuaishou.com>
2025-02-24 17:44:14 +0800

    feat: 添加electron-demo

---
+++ packages/electron-demo/index.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    // 创建浏览器窗口
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // 加载应用的 index.html
+    win.loadFile('index.html');
+}
+
+// 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
+app.whenReady().then(createWindow);
+
+// 当所有窗口关闭时退出应用
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
